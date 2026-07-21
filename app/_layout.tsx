370fabbed7a50396e330cd7c76c1a28a1434ab2d
s83805141-collab <s83805141@gmail.com>
2026-07-21 11:34:39 +0530

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,4 @@
-import { useEffect } from 'react';
-import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
