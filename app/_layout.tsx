02a77295b8d19cf165b4247f8af8e9f20ed4077f
s83805141-collab <s83805141@gmail.com>
2026-07-21 23:06:43 +0530

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,50 +1,5 @@
-import { useEffect } from 'react';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-
-import {
-  Roboto_400Regular,
-  Roboto_500Medium,
-  Roboto_700Bold,
-} from '@expo-google-fonts/roboto';
-
-import { useFrameworkReady } from '@/hooks/useFrameworkReady';
-import { MD3Colors } from '@/lib/theme';
-
-// SplashScreen.preventAutoHideAsync();
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  useFrameworkReady();
-
-  const [fontsLoaded, fontError] = useFonts({
-    'Roboto-Regular': Roboto_400Regular,
-    'Roboto-Medium': Roboto_500Medium,
-    'Roboto-Bold': Roboto_700Bold,
-  });
-
-  useEffect(() => {
-    async function hideSplash() {
-      if (fontsLoaded || fontError) {
-        // await SplashScreen.hideAsync();
-      }
-    }
-
-    hideSplash();
-  }, [fontsLoaded, fontError]);
-
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
-
-  return (
-  <>
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)" />
-      <Stack.Screen name="+not-found" />
-    </Stack>
-
-    <StatusBar style="dark" />
-  </>
-);
+  return <Stack screenOptions={{ headerShown: false }} />;
+}
