e6ee99191e7fbc8536ae8496973cda5c198ed674
s83805141-collab <s83805141@gmail.com>
2026-08-02 20:23:16 +0530

    Change FileSystem import to legacy version

--- app/backup.tsx
+++ app/backup.tsx
@@ -12,7 +12,7 @@ import {
 import { DatabaseBackup, Upload, FileJson, ShieldCheck, AlertCircle, Info, Share2 } from 'lucide-react-native';
 import { exportBackup, importBackup, downloadBackupFile } from '../lib/db/database';
 import * as DocumentPicker from 'expo-document-picker';
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 import { ScreenHeader } from '../components/ui';
 
 export default function BackupScreen() {
